cb0aab5d7dd7ff7ec498c152940f722414f6b168
Mehdi.Zare <mehdi.zare@nav.no>
2025-12-03 08:00:16 +0100

    Fjernet ubrukt testdata

--- src/test/kotlin/no/nav/helsearbeidsgiver/dialogporten/MockData.kt
+++ src/test/kotlin/no/nav/helsearbeidsgiver/dialogporten/MockData.kt
@@ -17,29 +17,4 @@ object MockData {
         )
 
     val gyldingRespons = "0194cb3a-6f4e-7707-a506-a1db2b5c37fa"
-
-    val transmissionsJson =
-        """
-        [
-               {
-                   "id": "0194cb3a-6f4e-7707-a506-a1db2b5c37fa",
-                   "type": "Information",
-                   "extendedType": "extendedType",
-                   "sender": {
-                       "actorType": "actorType"
-                   },
-                   "content": {
-                       "title": {
-                           "value": [
-                               {
-                                   "value": "title",
-                                   "languageCode": "nb"
-                               }
-                           ],
-                           "mediaType": "text/plain"
-                       }
-                   }
-               }
-           ]
-        """.trimIndent()
 }
