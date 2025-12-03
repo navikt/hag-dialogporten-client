b764dac5ef7b9fd10f866b0b0e035d789c051da2
Mehdi Zare <32900443+mettok@users.noreply.github.com>
2025-12-03 14:53:01 +0100

    Vedlegg (#23)

--- src/main/kotlin/no/nav/helsearbeidsgiver/dialogporten/domene/Transmission.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/dialogporten/domene/Transmission.kt
@@ -48,23 +48,4 @@ data class Transmission(
         // Update to previously submitted data
         Correction,
     }
-
-    @Serializable
-    data class Attachment(
-        val displayName: List<ContentValueItem>,
-        val urls: List<Url>,
-    )
-
-    @Serializable
-    data class Url(
-        val url: String,
-        val mediaType: String,
-        val consumerType: AttachmentUrlConsumerType,
-    )
-
-    @Serializable
-    enum class AttachmentUrlConsumerType {
-        Gui,
-        Api,
-    }
 }
--- src/main/kotlin/no/nav/helsearbeidsgiver/dialogporten/domene/TransmissionRequest.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/dialogporten/domene/TransmissionRequest.kt
@@ -1,55 +1,28 @@
 package no.nav.helsearbeidsgiver.dialogporten.domene
 
-import io.ktor.http.ContentType
 import java.util.UUID
 
 abstract class TransmissionRequest {
-    val vedleggMediaType = ContentType.Application.Json.toString()
     abstract val extendedType: String
     abstract val dokumentId: UUID
     abstract val tittel: String
     abstract val sammendrag: String?
-    abstract val vedleggNavn: String
-    abstract val vedleggBaseUrl: String
     abstract val type: Transmission.TransmissionType
     abstract val relatedTransmissionId: UUID?
+    abstract val attachments: List<Attachment>?
 }
 
-fun lagTransmissionMedVedlegg(transmissionRequest: TransmissionRequest): Transmission =
+fun TransmissionRequest.toTransmission(): Transmission =
     Transmission(
-        type = transmissionRequest.type,
-        extendedType = transmissionRequest.extendedType,
-        externalReference = transmissionRequest.dokumentId.toString(),
+        type = type,
+        extendedType = extendedType,
+        externalReference = dokumentId.toString(),
         sender = Transmission.Sender("ServiceOwner"),
-        relatedTransmissionId = transmissionRequest.relatedTransmissionId,
+        relatedTransmissionId = relatedTransmissionId,
         content =
             Content.create(
-                title = transmissionRequest.tittel,
-                summary = transmissionRequest.sammendrag,
+                title = tittel,
+                summary = sammendrag,
             ),
-        attachments =
-            listOf(
-                Transmission.Attachment(
-                    displayName = listOf(ContentValueItem(transmissionRequest.vedleggNavn)),
-                    urls =
-                        listOf(
-                            Transmission.Url(
-                                url = "${transmissionRequest.vedleggBaseUrl}/${transmissionRequest.dokumentId}",
-                                mediaType = transmissionRequest.vedleggMediaType,
-                                consumerType = Transmission.AttachmentUrlConsumerType.Api,
-                            ),
-                        ),
-                ),
-                Transmission.Attachment(
-                    displayName = listOf(ContentValueItem(transmissionRequest.vedleggNavn)),
-                    urls =
-                        listOf(
-                            Transmission.Url(
-                                url = transmissionRequest.vedleggBaseUrl,
-                                mediaType = transmissionRequest.vedleggMediaType,
-                                consumerType = Transmission.AttachmentUrlConsumerType.Gui,
-                            ),
-                        ),
-                ),
-            ),
+        attachments = attachments,
     )
