cc6128da2bff672d7078060366d902a245cf0952
Mehdi Zare <32900443+mettok@users.noreply.github.com>
2025-12-04 08:06:26 +0100

    sette attachments til ikke null (#24)

--- src/main/kotlin/no/nav/helsearbeidsgiver/dialogporten/domene/TransmissionRequest.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/dialogporten/domene/TransmissionRequest.kt
@@ -9,7 +9,7 @@ abstract class TransmissionRequest {
     abstract val sammendrag: String?
     abstract val type: Transmission.TransmissionType
     abstract val relatedTransmissionId: UUID?
-    abstract val attachments: List<Attachment>?
+    abstract val attachments: List<Attachment>
 }
 
 fun TransmissionRequest.toTransmission(): Transmission =
